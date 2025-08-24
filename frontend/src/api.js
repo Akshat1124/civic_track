a77577c6466cb3b6547b569124e0899cacc901a1
AKSHAT SRIVASTAVA <akshatsrivastava1124@gmail.com>
2025-08-25 04:45:06 +0530

    removed the extra axious line

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,14 +1,3 @@
-import axios from 'axios';
-
-// const API_URL = 'http://localhost:5000/api/complaint';
-
-// export const fileComplaint = (complaintData) => {
-//   return axios.post(`${API_URL}/file`, complaintData);
-// };
-
-// export const trackComplaint = (complaintId) => {
-//   return axios.get(`${API_URL}/track/${complaintId}`);
-// };
 
 
 
